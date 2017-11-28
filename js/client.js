d130e8d70b1bc1864556275caca45fe8a6c379b1
xtianonil <xtianonil@users.noreply.github.com>
2017-11-28 11:17:57 +0800

    Update client.js

--- js/client.js
+++ js/client.js
@@ -88,7 +88,7 @@ var getBadges = function(t){
   return t.card('name')
   .get('name')
   .then(function(cardName){
-    console.log('We just loaded the card name for fun: ' + cardName);
+    console.log('We just loaded the card name for fun: ' + cardName + " " + t.card('id'));
     
     return [{
       // dynamic badges can have their function rerun after a set number
