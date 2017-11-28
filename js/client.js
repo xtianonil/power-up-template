bb67b6e6936bceeeffdb28c199d20c867ca8cd9e
xtianonil <xtianonil@users.noreply.github.com>
2017-11-28 14:31:29 +0800

    Update client.js

--- js/client.js
+++ js/client.js
@@ -79,6 +79,8 @@ t.getAll();
 var GLITCH_ICON = './images/glitch.svg';
 var WHITE_ICON = './images/icon-white.svg';
 var GRAY_ICON = './images/icon-gray.svg';
+var estimatedTimeSelector = document.getElementById('estimatedTime');
+alert(estimatedTimeSelector.value);
 
 var randomBadgeColor = function() {
   return ['green', 'yellow', 'red', 'none'][Math.floor(Math.random() * 4)];
@@ -108,7 +110,7 @@ var getBadges = function(t){
       // you can mix and match between static and dynamic
       title: 'Detail Badge', // for detail badges only
       //text: estimatedTimeSelector.value,
-      text: 'Static',
+      text: 'Static2',
       icon: GRAY_ICON, // for card front badges only
       color: null
     }, {
