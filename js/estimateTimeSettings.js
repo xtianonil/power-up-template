94cb7a9a1aaf5159688ab1a0e0c2c247dc5fc666
xtianonil <xtianonil@users.noreply.github.com>
2017-12-10 21:45:53 +0800

    Update estimateTimeSettings.js

--- js/estimateTimeSettings.js
+++ js/estimateTimeSettings.js
@@ -6,12 +6,20 @@ var t = TrelloPowerUp.iframe();
 var estimateTimeHours = document.getElementById('estimateTimeHours');
 var estimateTimeMinutes = document.getElementById('estimateTimeMinutes');
 
+function formatHours(hours)
+{
+  if (hours < 1)
+    return "";
+  else
+    return hours + " hours";
+}
+
 function formatMinutes(minutes)
 {
   if (minutes < 1)
     return "";
   else
-    return minutes + " minutes";
+    return ", "+minutes + " minutes";
 }
 
 t.render(function(){
@@ -35,7 +43,7 @@ document.getElementById('saveEstimateTime').addEventListener('click', function()
     return t.set('card', 'shared', 'estimateTimeMinutes', estimateTimeMinutes.value);
   })
   .then(function(){
-    return t.set('card', 'shared', 'estimateTime', estimateTimeHours.value + " hours, " + formatMinutes(estimateTimeMinutes.value));
+    return t.set('card', 'shared', 'estimateTime', formatHours(estimateTimeHours.value) + formatMinutes(estimateTimeMinutes.value));
   })
   .then(function(){
     t.closePopup();
